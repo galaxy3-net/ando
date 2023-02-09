7fd551b6852dee004e64314e16698e8849a3a287
korbenkirscht <korben@kirscht.com>
2023-02-08 19:05:40 -0800

    Add openvpn

--- bin/openvpn.sh
+++ bin/openvpn.sh
@@ -2,6 +2,6 @@
 
 sudo apt install -y openvpn && \
 mkdir -p ~/openvpn && \
-wget https://configs.ipvanish.com/configs.zip -O ~/openvpn/configs.zip && \
+wget https://configs.ipvanish.com/simpleconf/configs.zip -O ~/openvpn/configs.zip && \
 (cd ~/openvpn/ && unzip configs.zip) && \
 wall 'vpn is ready to start using: sudo openvpn ~/openvpn/ipvanish-US-City-Abrev-instance.ovpn'
